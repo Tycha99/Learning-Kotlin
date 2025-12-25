0bc63340337ea2da27a6256aac810c00906f8e24
Danis <danis.digger@gmail.com>
2025-12-25 17:01:31 +0300

    Создание класса SmartTvDevice

--- Unit2/classes/createMethods.kt
+++ Unit2/classes/createMethods.kt
@@ -1,19 +1,27 @@
-class SmartDevice(val name: String, val category: String) {
+class SmartTvDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
 
-    var deviceStatus = "online"
+    var speakerVolume = 2
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
 
-    fun turnOn() {
-        println("Smart device is turned on.")
+    var channelNumber = 1
+        set(value) {
+            if (value in 0..200) {
+                field = value
+            }
+        }
+    
+    fun increaseSpeakerVolume() {
+        speakerVolume++
+        println("Speaker volume increased to $speakerVolume.")
     }
 
-    fun turnOff() {
-        println("Smart device is turned off.")
+    fun nextChannel() {
+        channelNumber++
+        println("Channel number increased to $channelNumber.")
     }
-}
-
-fun main() {
-    val smartTvDevice = SmartDevice()
-    println("Device name is: ${smartTvDevice.name}")
-    smartTvDevice.turnOn()
-    smartTvDevice.turnOff()
 }
