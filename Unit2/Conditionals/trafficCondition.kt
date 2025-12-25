85806e8f505802ae665c6ef016cc680274bff7e9
Danis <danis.digger@gmail.com>
2025-12-25 15:56:51 +0300

    улучшение условия светофора

--- Unit2/Conditionals/trafficCondition.kt
+++ Unit2/Conditionals/trafficCondition.kt
@@ -1,7 +1,11 @@
 fun main() {
-    val trafficLightColor = "Red"
+    val trafficLightColor = "Yellow"
 
     if (trafficLightColor == "Red") {
         println("Stop")
-    } 
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else {
+        println("Go")
+    }
 }
