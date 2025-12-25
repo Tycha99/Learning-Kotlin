2e224cd969b9b7dd5ea0faa9a94e956532e802f7
Danis <danis.digger@gmail.com>
2025-12-25 16:16:47 +0300

    Использование null в условии

--- Unit2/useNull/learnNull.kt
+++ Unit2/useNull/learnNull.kt
@@ -1,4 +1,9 @@
 fun main() {
     var favoriteActor: String? = null
-    println(favoriteActor?.length)
+
+    if(favoriteActor != null) {
+      println("The number of characters in your favorite actor's name is ${favoriteActor.length}.")
+    } else {
+      println("You didn't input a name.")
+    }
 }
