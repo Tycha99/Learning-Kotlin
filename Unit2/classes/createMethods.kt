4053227d5758bc36fea72bee6fa728d408ab2acb
Danis <danis.digger@gmail.com>
2025-12-25 17:12:52 +0300

    Переопределение методов суперкласса из подклассов

--- Unit2/classes/createMethods.kt
+++ Unit2/classes/createMethods.kt
@@ -1,3 +1,16 @@
+open class SmartDevice(val name: String, val category: String) {
+
+    var deviceStatus = "online"
+
+    open fun turnOn() {
+        // function body
+    }
+
+    open fun turnOff() {
+        // function body
+    }
+}
+
 class SmartTvDevice(deviceName: String, deviceCategory: String) :
     SmartDevice(name = deviceName, category = deviceCategory) {
 
@@ -14,7 +27,7 @@ class SmartTvDevice(deviceName: String, deviceCategory: String) :
                 field = value
             }
         }
-    
+
     fun increaseSpeakerVolume() {
         speakerVolume++
         println("Speaker volume increased to $speakerVolume.")
@@ -23,6 +36,19 @@ class SmartTvDevice(deviceName: String, deviceCategory: String) :
     fun nextChannel() {
         channelNumber++
         println("Channel number increased to $channelNumber.")
+    }
+
+    override fun turnOn() {
+        deviceStatus = "on"
+        println(
+            "$name is turned on. Speaker volume is set to $speakerVolume and channel number is " +
+                "set to $channelNumber."
+        )
+    }
+
+    override fun turnOff() {
+        deviceStatus = "off"
+        println("$name turned off")
     }
 }
 
@@ -40,8 +66,19 @@ class SmartLightDevice(deviceName: String, deviceCategory: String) :
         brightnessLevel++
         println("Brightness increased to $brightnessLevel.")
     }
-}
 
+    override fun turnOn() {
+        deviceStatus = "on"
+        brightnessLevel = 2
+        println("$name turned on. The brightness level is $brightnessLevel.")
+    }
+
+    override fun turnOff() {
+        deviceStatus = "off"
+        brightnessLevel = 0
+        println("Smart Light turned off")
+    }
+}
 
 class SmartHome(val smartTvDevice: SmartTvDevice,val smartLightDevice: SmartLightDevice) {
 
@@ -81,4 +118,12 @@ class SmartHome(val smartTvDevice: SmartTvDevice,val smartLightDevice: SmartLigh
         turnOffTv()
         turnOffLight()
     }
+}
+
+fun main() {
+    var smartDevice: SmartDevice = SmartTvDevice("Android TV", "Entertainment")
+    smartDevice.turnOn()
+    
+    smartDevice = SmartLightDevice("Google Light", "Utility")
+    smartDevice.turnOn()
 }
