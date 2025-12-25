ac2aba46babc39c95429a451feeb66ee19714239
Danis <danis.digger@gmail.com>
2025-12-25 16:18:37 +0300

    Использование null в выражении if

--- Unit2/useNull/learnNull.kt
+++ Unit2/useNull/learnNull.kt
@@ -1,9 +1,11 @@
 fun main() {
-    var favoriteActor: String? = null
+    var favoriteActor: String? = "Sandra Oh"
 
-    if(favoriteActor != null) {
-      println("The number of characters in your favorite actor's name is ${favoriteActor.length}.")
+    val lengthOfName = if (favoriteActor != null) {
+      favoriteActor.length
     } else {
-      println("You didn't input a name.")
+      0
     }
+
+    println("The number of characters in your favorite actor's name is $lengthOfName.")
 }
