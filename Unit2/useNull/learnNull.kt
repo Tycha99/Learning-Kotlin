6add7b1fb37c2e78f8dbfbc350d74734a7edde1c
Danis <danis.digger@gmail.com>
2025-12-25 16:19:25 +0300

    Использование тернарного оператора

--- Unit2/useNull/learnNull.kt
+++ Unit2/useNull/learnNull.kt
@@ -1,11 +1,7 @@
 fun main() {
     var favoriteActor: String? = "Sandra Oh"
 
-    val lengthOfName = if (favoriteActor != null) {
-      favoriteActor.length
-    } else {
-      0
-    }
+    val lengthOfName = favoriteActor?.length ?: 0
 
     println("The number of characters in your favorite actor's name is $lengthOfName.")
 }
